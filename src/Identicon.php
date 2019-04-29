d2cffb6c6a1b6efb4860d427726dacb2901717ad
Tim Hinz <tim.hinz@guns.com>
2019-04-29 13:39:12 -0500

    Initial Commit

---
+++ src/Identicon.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Identicon.php
+ *
+ * Generates a image based on a given input string to
+ * be used as an avatar.
+ *
+ * Copyright 2019 Tim Hinz, All rights reserved.
+ * ====================================================
+ * Date: 2019-04-29
+ * Repo: https://github.com/Celtech/Identicon-Generator
+ */
+
+class Identicon
+{
+
+}
